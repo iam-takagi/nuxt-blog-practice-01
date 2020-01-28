374c80bd1f2fd85c9b2ff4470c30c30b9617666c
notsmatch <smatchjp@gmail.com>
2020-01-28 18:49:48 +0900

    gg

--- nuxt.config.js
+++ nuxt.config.js
@@ -24,8 +24,8 @@ const config = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' },
-      { hid: 'og:description', property: 'og:description',content: process.env.npm_package_description || ''  },
+      //{ hid: 'description', name: 'description', content: process.env.npm_package_description || '' },
+      //{ hid: 'og:description', property: 'og:description',content: process.env.npm_package_description || ''  },
       { hid: 'og:url', property: 'og:url', content: 'https://mctech.life/' },
       { hid: 'og:title', property: 'og:title', content: 'Miencraft Tech Portal JP' },
       { hid: 'og:type', property: 'og:type', content: 'website'},
